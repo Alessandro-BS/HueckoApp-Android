e217349b4e4945dbde60ee9fe9fd5ad5d1c38503
Alessandro-BS <fabrizio.bs9012@gmail.com>
2026-09-11 02:57:17 -0500

    README.md actualizado

--- app/src/main/java/com/example/hueckoapp/MainActivity.kt
+++ app/src/main/java/com/example/hueckoapp/MainActivity.kt
@@ -13,6 +13,7 @@ import com.example.hueckoapp.ui.theme.HueckoAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        android.util.Log.d("MainActivity", "onCreate called")
         setContent {
             HueckoAppTheme {
                 Surface(
@@ -23,5 +24,30 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        android.util.Log.d("MainActivity", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        android.util.Log.d("MainActivity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        android.util.Log.d("MainActivity", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        android.util.Log.d("MainActivity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        android.util.Log.d("MainActivity", "onDestroy called")
     }
 }
